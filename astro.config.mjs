68f764b62b746730bd3e252d74e5374845c5f41a
josue Huayapa <huayapajosue0@gmail.com>
2025-01-03 18:51:23 -0500

    config env and output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: "/",
+  env: {
+    schema: {
+      RESEND_API_KEY: envField.string({
+        context: "server",
+        access: "secret",
+        startsWith: "re_"
+      }),
+      DOMAIN_URL: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    }
+  },
+  output: "server",
+});
